abbbc1904d9a66dad97e29f00ec9b92f3a399cd4
CattleHoon <samhoon1128@gmail.com>
2025-09-18 21:13:37 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	while (true) {
+		int a, b;
+		cin >> a >> b;
+		if (a == 0 && b == 0) break;
+		cout << a + b << '\n';
+	}
+	return 0;
+}
