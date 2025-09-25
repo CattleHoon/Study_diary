d28573be0a88401d8326d7918b0d9a9bcb38339c
CattleHoon <samhoon1128@gmail.com>
2025-09-25 20:28:02 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	string s;
+	cin >> s;
+	cout << s.size() << '\n';
+
+	return 0;
+}
