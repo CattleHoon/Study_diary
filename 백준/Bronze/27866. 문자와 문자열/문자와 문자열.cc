75878141369c65c7da31e02c2c19388ff7c5d88c
CattleHoon <samhoon1128@gmail.com>
2025-09-25 20:24:29 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+#include<vector>
+using namespace std;
+
+int main() {
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+
+	string s;
+	cin >> s;
+	int i = 0;
+	cin >> i;
+	cout << s[i-1] << '\n';
+
+	return 0;
+}
